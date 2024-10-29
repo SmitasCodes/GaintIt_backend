b39c3ac57166f359bd708870935cf3e184714521
SmitasCodes <airidassmith30@gmail.com>
2024-10-29 14:21:34 +0200

    added user model

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Connected to DB ${connect.connection.host}`);
+    console.log(`Connected to DB ${connect.connection.host}.`);
   } catch (err) {
     console.error(`Could not connect to DB ${err}`);
   }
---
+++ models/usersModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema(
+    {
+      username: {
+        type: String,
+        required: [true, "Please add a username"],
+        unique: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: [true, "Please add a email"],
+        unique: true,
+        trim: true,
+        match: [
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+          "Please add a valid email",
+        ],
+      },
+      password: {
+        type: String,
+        required: [true, "Please add a password"],
+      },
+      role: {
+        type: String,
+        enum: ["user", "admin"], // Specify allowed roles
+        default: "user",
+      },
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+module.exports = User;
