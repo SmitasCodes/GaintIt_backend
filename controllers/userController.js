031ab7a3c5b5fe306989ba4c469549fd43b82972
SmitasCodes <airidassmith30@gmail.com>
2025-01-20 12:41:10 +0200

    records/users controller clean up, setting proper error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
-const Demo = require("../models/demoModel");
 const asyncHandler = require("express-async-handler");
 
 //======================== REGISTER USER ========================//
@@ -16,46 +15,41 @@ const registerUser = (Model) =>
 
     if (!username || !email || !password) {
       res.status(400);
-      throw new Error("Please enter all fields");
+      throw new Error("Missing required fields");
     }
 
-    try {
-      const emailExist = await Model.findOne({ email });
-      if (emailExist) {
-        res.status(400);
-        throw new Error("User with same email exist");
-      }
+    const emailExist = await Model.findOne({ email });
+    if (emailExist) {
+      res.status(400);
+      throw new Error("User with the same email exist");
+    }
 
-      const usernameExists = await Model.findOne({ username });
-      if (usernameExists) {
-        res.status(400);
-        throw new Error("User with same username exist");
-      }
+    const usernameExists = await Model.findOne({ username });
+    if (usernameExists) {
+      res.status(400);
+      throw new Error("User with the same username exist");
+    }
 
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-      const user = await Model.create({
-        username,
-        email,
-        password: hashedPassword,
+    const user = await Model.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id),
+        role: user.role,
       });
-
-      if (user) {
-        res.status(201).json({
-          _id: user.id,
-          username: user.username,
-          email: user.email,
-          token: generateToken(user._id),
-          role: user.role,
-        });
-      } else {
-        res.status(400);
-        throw new Error("Invalid user data");
-      }
-    } catch (error) {
-      res.status(500);
-      console.error(error.message);
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
     }
   });
 
@@ -68,24 +62,19 @@ const registerUser = (Model) =>
 const loginUser = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
 
-  try {
-    const user = await User.findOne({ username });
+  const user = await User.findOne({ username });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      res.status(200).json({
-        _id: user.id,
-        username: user.username,
-        email: user.email,
-        role: user.role,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(400);
-      throw new Error("Invalid credentials");
-    }
-  } catch (error) {
-    res.status(500);
-    console.log(error.message);
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
   }
 });
 
--- controllers/workoutRecordController.js
+++ controllers/workoutRecordController.js
@@ -18,70 +18,62 @@ const addWorkoutRecord = asyncHandler(async (req, res) => {
     throw new Error("Template ID or user ID is not valid");
   }
 
-  try {
-    const template = await WorkoutTemplate.findOne({
-      _id: template_id,
-      user_id,
-    });
-    if (!template) {
-      res.status(404);
-      throw new Error(
-        "Workout template not found or does not belong to this user."
-      );
-    }
-
-    // Exercises validation, checking if exercises names matches templates exercises names. Also checking if reps/weight/sets are positive and if reps array length is equal to sets length.
-    const templateExercisesNames = template.exercises.map(
-      (e) => e.exercise_name
-    );
-    const invalidExercisesNames = exercises.filter(
-      (e) => !templateExercisesNames.includes(e.exercise_name)
-    );
-
-    if (invalidExercisesNames.length > 0) {
-      res.status(400);
-      throw new Error("Some exercises do not match the selected template.");
-    }
-
-    const invalidExercisesData = exercises.filter(
-      (e) =>
-        !e.weight ||
-        !e.reps ||
-        !e.sets ||
-        e.weight <= 0 ||
-        e.reps <= 0 ||
-        e.sets <= 0 ||
-        e.reps.length != e.sets
-    );
-
-    if (invalidExercisesData.length > 0) {
-      res.status(400);
-      throw new Error("Some exercises have invalid data (weight, reps, sets).");
-    }
-
-    let date = workout_date ? new Date(workout_date) : new Date();
-    if (isNaN(date.getTime())) {
-      throw new Error("Invalid date provided.");
-    }
-
-    const workoutRecord = await WorkoutRecord.create({
-      template_id,
-      template_name: template.name,
-      user_id,
-      exercises,
-      workout_date: date,
-    });
-
-    if (!workoutRecord) {
-      res.status(500).json("Failed to create workout record.");
-      return;
-    }
-
-    res.status(201).json({ workoutRecord });
-  } catch (error) {
-    res.status(500);
-    console.error(error.message);
+  const template = await WorkoutTemplate.findOne({
+    _id: template_id,
+    user_id,
+  });
+  if (!template) {
+    res.status(404);
+    throw new Error("Workout template not found.");
   }
+
+  // Exercises validation, checking if exercises names matches templates exercises names. Also checking if reps/weight/sets are positive and if reps array length is equal to sets length.
+  const templateExercisesNames = template.exercises.map((e) => e.exercise_name);
+  const invalidExercisesNames = exercises.filter(
+    (e) => !templateExercisesNames.includes(e.exercise_name)
+  );
+
+  if (invalidExercisesNames.length > 0) {
+    res.status(400);
+    throw new Error("Exercises do not match the selected template.");
+  }
+
+  const invalidExercisesData = exercises.filter(
+    (e) =>
+      !e.weight ||
+      !e.reps ||
+      !e.sets ||
+      e.weight <= 0 ||
+      e.reps <= 0 ||
+      e.sets <= 0 ||
+      e.reps.length != e.sets
+  );
+
+  if (invalidExercisesData.length > 0) {
+    res.status(400);
+    throw new Error("Some exercises have invalid data (weight, reps, sets).");
+  }
+
+  let date = workout_date ? new Date(workout_date) : new Date();
+  if (isNaN(date.getTime())) {
+    res.status(400);
+    throw new Error("Invalid date provided.");
+  }
+
+  const workoutRecord = await WorkoutRecord.create({
+    template_id,
+    template_name: template.name,
+    user_id,
+    exercises,
+    workout_date: date,
+  });
+
+  if (!workoutRecord) {
+    res.status(500);
+    throw new Error("Failed to create new record");
+  }
+
+  res.status(201).json({ workoutRecord });
 });
 
 //======================== GET WORKOUTS RECORDS ========================//
@@ -99,16 +91,15 @@ const getUserWorkoutRecords = asyncHandler(async (req, res) => {
   }
 
   const workoutRecords = await WorkoutRecord.find({ user_id });
-   
-  if (workoutRecords && workoutRecords.length > 0) {
-    res.status(200).json({
-      records: workoutRecords,
-    });
-  } else {
-    // res.status(404);
+
+  if (!workoutRecords || workoutRecords.length === 0) {
+    res.status(404);
     throw new Error("No workout records found");
   }
 
+  res.status(200).json({
+    records: workoutRecords,
+  });
 });
 
 //======================== DELETE WORKOUT RECORD ========================//
@@ -126,22 +117,17 @@ const deleteWorkoutRecord = asyncHandler(async (req, res) => {
     throw new Error("Record ID or user ID is not valid");
   }
 
-  try {
-    const deleteRecord = await WorkoutRecord.deleteOne({
-      user_id,
-      _id: record_id,
-    });
+  const deleteRecord = await WorkoutRecord.deleteOne({
+    user_id,
+    _id: record_id,
+  });
 
-    if (deleteRecord.deletedCount === 0) {
-      res.status(404);
-      throw new Error("Workout record not found");
-    }
-
-    res.status(200).json({ message: "Workout record deleted" });
-  } catch (error) {
-    res.status(500);
-    throw new Error(error.message);
+  if (deleteRecord.deletedCount === 0) {
+    res.status(404);
+    throw new Error("Workout record not found");
   }
+
+  res.status(200).json({ message: "Workout record deleted" });
 });
 
 module.exports = {
